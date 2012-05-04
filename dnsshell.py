d5f682b82cbb8fa4975edb125ff9c8972f8dbdff
Florian Streibelt <florian@freitagsrunde.org>
2012-05-04 23:40:40 +0200

    Initial Codefragments

---
+++ dnsshell.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# dnsshell  - allows Zone-configuration of BIND by untrusted users
+#
+# Download: https://github.com/mutax/DNS-Shell
+#
+# Copyright (C) 2012 Florian Streibelt <florian@freitagsrunde.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import signal
+import argparse
+import cmd
+import sys
+import ConfigParser
+
+from lib import Cmdloops
+
+def handler(signum, frame):
+	"""Just do nothing"""
+
+def main():
+
+
+	parser = argparse.ArgumentParser(description='Shell to edit BIND-Zones by untrusted users.')
+	parser.add_argument('-v','--version', action='version', version='%(prog)s ' + "  version " +Cmdloops.DS_base.__version__,
+						help="displays version and exits.")
+	parser.add_argument('-r', '--readonly', action="store_true", default=False, \
+                        help="readonly access to config.",dest='readonly')
+	parser.add_argument('-c', '--createconf', action="store_true", default=False, \
+                        help="create sample config in <file>.",dest='createconf')
+	parser.add_argument('configfile', metavar='<file>', type=str,\
+						help="configfile, will be created if -c given.")
+
+	args = parser.parse_args()
+
+	configfile=args.configfile
+
+	config = ConfigParser.RawConfigParser()
+
+	mode="r+"
+	if args.readonly:
+		mode="r"
+
+	try:
+		open(configfile, mode)
+		print "opened %s in mode %s" % (configfile, mode)
+	except IOError as (errno, strerror):
+		print "I/O error(%i) opening '%s': %s" %(errno, configfile, strerror)
+
+		if ((errno==2)&(not args.readonly)):
+			#Not found: Create a new, template config
+			print "about to create a config in %s\n" %configfile
+			#TODO: create config-template/ask user questions.
+			#      maybe special mode 'unconfigured>' ;o)
+			u = Cmdloops.InitialConfig(config)
+			u.wizard()
+			c = Cmdloops.DS_config()
+			c.setBaseConfig(u.firstname, u.lastname, u.email)
+			pass
+
+		if ((errno==13)&(not args.readonly)):
+			#Permission denied:
+			print "Permission denied writing %s\n" %configfile
+
+		sys.exit(1)
+
+
+
+	# Install Ctrl+C Handler
+	signal.signal(signal.SIGINT, handler)
+
+	d = Cmdloops.DNSShell()
+	# FIXME: give cmdlineargs to class/make them global/...
+	d.cmdloop()
+
+
+if __name__ == '__main__':
+	main()
---
+++ lib/Cmdloops.py
@@ -0,0 +1,156 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2012 Florian Streibelt <florian@freitagsrunde.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import argparse
+import cmd
+from email.utils import parseaddr
+
+
+class DS_base(cmd.Cmd):
+	"""DNS-Shell base command loop Class"""
+
+	__version__ = '0.0.1'
+
+	def __init__(self):
+		self.doc_header="Available commands (type ?<topic>):"
+		self.prompt="> "
+		self.intro=""
+		self.intro+="\nuse ? for help."
+
+	def get_version(self):
+		return __version__
+
+	def do_help(self, args):
+		"""Get help on commands
+		'help' or '?' with no arguments prints a list of commands for which help is available
+		'help <command>' or '? <command>' gives help on <command>
+		"""
+		## The only reason to define this method is for the help text in the doc string
+		cmd.Cmd.do_help(self, args)
+
+	def do_hist(self, args):
+		"""Print a list of commands that have been entered"""
+		for line in self._hist:
+			print " "+line
+
+	def emptyline(self):
+		"""Do nothing on empty input line"""
+		pass
+
+	def preloop(self):
+		"""Initialize command history and call parent class func"""
+		cmd.Cmd.preloop(self)   ## sets up command completion
+		self._hist    = []      ## No history yet
+
+	def precmd(self, line):
+		command = line.strip()
+		if line.strip():
+			self._hist += [ line.strip() ]
+		return line
+
+
+class DNSShell(DS_base):
+	"""DNS-Shell main command loop"""
+
+	def __init__(self):
+		intro="Welcome to dnsshell %s" % DS_base.__version__
+
+	def do_exit(self, line):
+		"exits the shell without saving."
+		return True
+
+	def do_enable(self, line):
+		"enters enable mode."
+		es=DS_enable()
+		es.cmdloop()
+
+	def do_ls(self, line):
+		"prints all configured domains"
+		print "Domains configured: "
+
+	def postloop(self):
+		print "Goodbye."
+
+
+
+class DS_enable(DS_base):
+	"""DNS-Shell main command loop"""
+
+	def __init__(self):
+		intro="You are now in enable mode"
+		prompt="enable> "
+
+	def do_return(self, line):
+		"exits the shell without saving."
+		return True
+
+
+
+class DS_config(DS_base):
+
+	def __init__(self):
+		intro="You may change your settings here. \nPlease use save to make changes permanent."
+		prompt="config> "
+		firstname=None
+		lastname=None
+		email=None
+
+	def setBaseConfig(self,first,last,mail):
+		self.firstname=first
+		self.lastname=last
+		self.email=mail
+
+class InitialConfig():
+
+	def __init__(self,config):
+		if (not config.type("ConfigParser")):
+
+		firstname = None
+		lastname = None
+		email = None
+
+	def wizard(self):
+		print "Initial configuration dialog"
+		print "----------------------------"
+
+		self.firstname = self._askStr("First Name.: ")
+		self.lastname  = self._askStr("Last Name..: ")
+		self.email     = self._askStr("Email......: ",tpe="email")
+
+
+	def _askStr(self,question,tpe="any"):
+		answer=None
+		while (not answer):
+			answer=raw_input(question)
+			if (not answer):
+				print("Value required")
+			elif (tpe=="email"):
+					addr = parseaddr(answer)[1]
+					if (addr):
+						answer=addr
+					else:
+						print("Illegal Email address:" + answer)
+						answer=None
+
+
+
+
+
+
